7d14fa5b6526012788ebcdb42c301803f95e2c92
caroline <syokaucaroline7@gmail.com>
2023-08-10 14:34:55 +0300

    ✨feat: created a library to hold card components✨created projects library

---
+++ libs/elements/cards/src/lib/elements-cards.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaProjectItemCardComponent } from './elewa-project-item-card/elewa-project-item-card.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaProjectItemCardComponent],
+  exports: [ElewaProjectItemCardComponent]
+})
+export class ElementsCardsModule {}
